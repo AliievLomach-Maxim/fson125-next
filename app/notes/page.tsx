9fa658a0623a95e64eedb135d4a54a6eccfb141b
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2026-05-20 20:59:56 +0300

    +

---
+++ app/notes/page.tsx
@@ -0,0 +1,38 @@
+const Notes = () => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod illum veniam alias in, dolores
+        quo dicta dolorem ipsam, perspiciatis pariatur voluptatem laudantium? Quaerat blanditiis
+        odit dolores non similique, quae aperiam! Expedita dicta laboriosam laudantium commodi
+        minus? Corrupti sint assumenda corporis nihil, expedita voluptate maxime provident, ut
+        possimus delectus impedit temporibus reiciendis natus debitis aut eius exercitationem
+        tenetur adipisci voluptatum. Culpa. Autem similique vero veniam deserunt officiis, quasi
+        impedit facere excepturi a eaque ab esse delectus. Voluptatum hic recusandae facilis
+        pariatur dicta ipsum, ut veniam iure veritatis eum blanditiis, labore placeat. Provident,
+        repellendus! Odio ut perspiciatis animi nobis reiciendis, provident expedita facere tempora
+        cupiditate perferendis autem fugiat nihil delectus sed, natus deserunt earum a eius neque
+        quos veniam rem? Provident, voluptates? Error repudiandae, ut quisquam suscipit quia
+        consectetur culpa necessitatibus iure animi aliquid, facilis voluptates deserunt? Ullam
+        asperiores veritatis iste maxime porro, reprehenderit sed fugiat itaque ex provident quos,
+        saepe ipsam! Sapiente nemo incidunt maxime, optio, expedita nesciunt debitis quas totam
+        laudantium enim eum voluptas cupiditate quo minima suscipit blanditiis quaerat tempore odio?
+        In, doloribus! Eveniet natus obcaecati sint labore. Harum. Eveniet ullam cum magni pariatur
+        provident nulla cumque consequuntur enim blanditiis. Eum, laudantium velit debitis porro
+        fuga alias quod pariatur obcaecati minima dicta? Veniam, dicta laborum. Nulla, laboriosam
+        cum? Impedit. A dolorum error, ipsa voluptatum provident reiciendis consequuntur, et quos
+        pariatur sed quaerat corporis at facilis, ratione aliquid incidunt hic quidem non optio
+        adipisci laboriosam doloremque. Ipsam cupiditate quisquam perferendis. Eos, sapiente nihil
+        odit ipsa nobis eius illum esse dolores, exercitationem quasi aut quod ab. Minus in
+        repellendus neque velit. Tempore ducimus accusantium veritatis voluptas voluptates, in quos
+        obcaecati assumenda. Consectetur, illo culpa sequi totam unde eius! Delectus in earum
+        doloremque molestiae voluptates ea blanditiis, itaque voluptatibus! Rerum, quisquam magnam!
+        Sit ipsa atque, eaque voluptates labore culpa similique! Animi, sequi.
+      </p>
+    </div>
+  )
+}
+
+export default Notes
